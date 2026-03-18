a9aa6448e39e567d8e97ff4b0a7fd4a152b32672
X23752475 <148968196+X23752475@users.noreply.github.com>
2026-03-18 11:47:24 +0000

    Create eBike.java

---
+++ eBike.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mobilitydevice;
+
+/**
+ *
+ * @author danie
+ */
+public class eBike extends MobilityDevice {
+    
+    private int maxSpeed;
+
+    // Constructor
+    public eBike(String deviceID, String location, int battery, boolean available, int maxSpeed) {
+        super(deviceID, "E-Bike", location, battery, available);
+        this.maxSpeed = maxSpeed;
+    }
+
+    // Getter
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    // Setter
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+   
+    @Override
+    public String displayInfo() {
+        return super.displayInfo() + ", Max Speed: " + maxSpeed + " km/h";
+    }
+    
+}
